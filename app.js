3c848a51f58fa2a9c9e322839d1fc72c04fd561b
ralph <ralph@pencilmedia.com>
2018-07-01 00:42:10 -0700

    x

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ const app = express();
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const port = process.env.PORT || '3001';
