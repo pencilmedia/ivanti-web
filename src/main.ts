84ae62dd0b9d472775c76ae39a259c459aac0e4c
ralph <ralph@pencilmedia.com>
2018-07-02 00:28:05 -0700

    x

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { enableProdMode } from '@angular/core';
+import './polyfills.ts';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
+import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
+import { AppModule } from './app/app.module';
 
 if (environment.production) {
   enableProdMode();
